ee6fa094bd1eaf8bf77a3c53f6ecfed89366f3d2
AKuznetsov <akuznetsov@gridgain.com>
2015-07-27 11:36:59 +0700

    IGNITE-843: WIP Generate cache type metadata configs.

--- modules/web-control-center/src/main/js/db.js
+++ modules/web-control-center/src/main/js/db.js
@@ -124,7 +124,7 @@ var CacheSchema = new Schema({
     rebalanceTimeout: Number,
     rebalanceThrottle: Number,
 
-    storeMetadatas: [{type: ObjectId, ref: 'CacheTypeMetadata'}],
+    storeMetadata: [{type: ObjectId, ref: 'CacheTypeMetadata'}],
     cacheStoreFactory: {
         kind: {
             type: String,
@@ -168,7 +168,7 @@ var CacheSchema = new Schema({
     sqlEscapeAll: Boolean,
     sqlOnheapRowCacheSize: Number,
     longQueryWarningTimeout: Number,
-    queryMetadatas: [{type: ObjectId, ref: 'CacheTypeMetadata'}],
+    queryMetadata: [{type: ObjectId, ref: 'CacheTypeMetadata'}],
     indexedTypes: [{keyClass: String, valueClass: String}],
     sqlFunctionClasses: [String],
     statisticsEnabled: Boolean,
